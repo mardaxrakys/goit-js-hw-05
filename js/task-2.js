21e2a263d6acde010b1a66c61b30dd39b9a6a994
mardaxrakys <mardaxrakys@gmail.com>
2025-01-31 13:13:29 +0200

    1

--- js/task-2.js
+++ js/task-2.js
@@ -1,20 +1,24 @@
 const getUsersWithFriend = (users, friendName) =>
   users.filter((user) => user.friends.includes(friendName));
 
-// Перевірка:
+// Перевірка
 const allUsers = [
-  { name: "Moore Hensley", friends: ["Sharron Pace"] },
-  { name: "Sharlene Bush", friends: ["Briana Decker", "Sharron Pace"] },
+  { name: 'Moore Hensley', friends: ['Sharron Pace'] },
+  { name: 'Sharlene Bush', friends: ['Briana Decker', 'Sharron Pace'] },
   {
-    name: "Ross Vazquez",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    name: 'Ross Vazquez',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
   },
-  { name: "Elma Head", friends: ["Goldie Gentry", "Aisha Tran"] },
-  { name: "Carey Barr", friends: ["Jordan Sampson", "Eddie Strong"] },
-  { name: "Blackburn Dotson", friends: ["Jacklyn Lucas", "Linda Chapman"] },
-  { name: "Sheree Anthony", friends: ["Goldie Gentry", "Briana Decker"] },
+  { name: 'Elma Head', friends: ['Goldie Gentry', 'Aisha Tran'] },
+  { name: 'Carey Barr', friends: ['Jordan Sampson', 'Eddie Strong'] },
+  { name: 'Blackburn Dotson', friends: ['Jacklyn Lucas', 'Linda Chapman'] },
+  { name: 'Sheree Anthony', friends: ['Goldie Gentry', 'Briana Decker'] },
 ];
 
-console.log(getUsersWithFriend(allUsers, "Briana Decker"));
-console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-console.log(getUsersWithFriend(allUsers, "Adrian Cross"));
+console.log(getUsersWithFriend(allUsers, 'Briana Decker'));
+// Очікуваний результат: [{ name: "Sharlene Bush", friends: ["Briana Decker", "Sharron Pace"] }, { name: "Sheree Anthony", friends: ["Goldie Gentry", "Briana Decker"] }]
+
+console.log(getUsersWithFriend(allUsers, 'Goldie Gentry'));
+// Очікуваний результат: [{ name: "Elma Head", friends: ["Goldie Gentry", "Aisha Tran"] }, { name: "Sheree Anthony", friends: ["Goldie Gentry", "Briana Decker"] }]
+
+console.log(getUsersWithFriend(allUsers, 'Adrian Cross')); // Очікуваний результат: []
